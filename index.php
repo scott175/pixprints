a8a390c80e3b54eb0f1f85b75b5f0c3b1c12a4a0
scott175 <79629941+scott175@users.noreply.github.com>
2022-07-04 21:18:40 +1200

    Added Header
    Added header and with links to other pages and is fully responsive that turns into a hamburger menu when that screen gets to small

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Pix prints </title>
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+
+</head>
+<body bgcolor="020017">
+    <nav>
+        <img class="logo" src="img/Pix-Prints-Logo.webp" alt="logo">
+
+        <ul class="nav-links">
+            <li>
+                <a href="index.php">HOME</a>
+            </li>
+            <li>
+                <a href="gallery.php">GALLERY</a>
+            </li>
+            <li>
+                <a href="about.php">ABOUT</a>
+            </li>  
+        </ul>
+
+        <div class="burger">
+           <div class="line1"></div>
+           <div class="line2"></div>
+           <div class="line3"></div>
+       </div>
+ 
+   </nav>
+ 
+
+ 
+
+
+
+
+    <script src="app.js"> </script>
+
+
+
+</body>
+</html>
