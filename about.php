bead1755b5a91e45db85c4afc390e2e55a7b7749
scott175 <79629941+scott175@users.noreply.github.com>
2022-07-31 12:54:22 +1200

    Added about us section to the about us page which explains the business

--- about.php
+++ about.php
@@ -18,7 +18,21 @@
 
     <?php include("nav-bar.php"); ?>
     
+    <div class="gallery-header">
+        
+        <?php echo "<a>$title1</a>";
+        ?>
+             
+    <a style="color: white; margin: 450px; font-size: 100px; font-family: 'Montserrat', sans-serif;"> ABOUT US</a>
+    </div>
 
+    <div class="about-us-p">
+        
+        
+        <p>We are a year 13 business start up from Mahurangi College, our goal is to provide beautiful prints of our local environment to help encourage preservation of the enviroment. We take photos and print them our selves mostly of the Rodney area in places such as Omaha, Point wells, Matakana, Warkworth and others. If you are interested in one of our high quality prints please check out our gallery of photos and to see more of what we do.
+        </p>
+        
+    </div>
     
 
     <body bgcolor="020017">
